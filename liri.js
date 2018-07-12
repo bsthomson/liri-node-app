921a88ca3e180e678c63d37e22d5ad49f5397b12
bsthomson <thomson.bs@gmail.com>
2018-07-12 18:15:01 -0500

    changes

--- liri.js
+++ liri.js
@@ -1,4 +1,16 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
+
+if (process.argv[2] === "my-tweets") {
+    console.log("Tweets, Yeah!");
+} else if (process.argv[2] === "spotify-this-song") {
+    console.log("Songs, Yeah!");
+} else if (process.argv[2] === "movie-this") {
+    console.log("Movies, Yeah!");
+} else if (process.argv[2] === "do-what-it-says") {
+    console.log("do-what-it-says");
+} else {
+    console.log("You speak gibberish and I wnat nothing to do with it");
+};
